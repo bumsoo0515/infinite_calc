77af20e9cbdd3f96bd7d22ffa4ded89c0251a1a9
Bumsoo Kim <bumsoo0515@gmail.com>
2024-12-19 00:27:11 +0900

    stdin으로 입력받는 경우

--- main.c
+++ main.c
@@ -6,34 +6,33 @@ int main() {
     Stack infix; infix.top = NULL;
     Stack postfix; postfix.top = NULL;
 
-    FILE *fp = fopen("input.txt","r");
+    // FILE *fp = fopen("input.txt","r");
+    FILE *fp = stdin;
     if (fp == NULL) {
         printf("Failed to open file");
         exit(1);
     }
 
+    printf("\n---- Infinite Calculator ----\n");
+    printf("Input:\n");
     node *inp = new_list();
     while (1) {
         char c = getc(fp);
         if (c == EOF) break;
+        putchar(c);
         push_back(inp, c);
     }
     fclose(fp);
 
     list_tokenize(inp, &infix);
-
-    // while (!stempty(&infix)) {
-    //     if (is_num(peek(&infix))) print_fraction(pop_num(&infix), -1);
-    //     else printf("%c", pop_op(&infix));
-    //     printf("\n");
-    // }
-    // return 0;
-    
     infix_to_postfix(&infix, &postfix);
 
-
     fraction *res = calculate(&postfix);
+    
+    printf("\n\nOutput:\n");
     print_fraction(res, PRINT_LIMIT);
+    printf("\n");
+
     free_fraction(res);
     free_list(inp);
 
